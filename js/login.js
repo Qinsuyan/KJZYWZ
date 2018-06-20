c09d9a50ad9ae812045b6039391999d70bb1cb09
GreeeenPepper <wuyize1994@163.com>
2018-06-21 02:00:42 +0800

    Picture Upload FrontEnd

--- js/login.js
+++ js/login.js
@@ -73,6 +73,7 @@ function Login()
 }
 
 function Register(){
+    var successflag = 1;
     var data = $("input[type='checkbox']").is(":checked");//返回结果：1
     if(data == false){
         $(" p[ id='warning-text'] ").css("visibility","visible");
@@ -83,45 +84,43 @@ function Register(){
     var SelfDescription = $(" textarea[ id='SelfDescription'] ").val();
     if(UserName==""||UserName==null){
         alert("用户名不能为空！");
-        flag = 0;
+        successflag = 0;
     }
     else{
         if(PassWord==""||PassWord==null){
             alert("密码不能为空！");
-            flag = 0;
+            successflag = 0;
         }
     }
     console.log(UserName);
     console.log(PassWord);
     console.log(SelfDescription);
-    var formData = new FormData();
-    formData.append("user_name",UserName);
-    formData.append("user_passwd",PassWord);
-    formData.append("")
 
-    $.ajax({
-        type: 'POST',
-        url: server,
-        data: formData
-        contentType: false,
-        async: false,
-        cache: false,
-        processData: false,s
-        success: function (data) {
 
-            if (data.status == "0") {
-                alert("SUcces.");
 
-            }
-            else if (data.status == "-1") {
-                alert("Error");
-            }
-            return true;
-        }
-    });
-
+    $("#image_upload").fileinput({
+        uploadUrl:"http://149.28.199.19:8888/user/",
+        allowedFileExtensions: ['jpg', 'png'],//接收的文件后缀
+        uploadExtraData:{"name": UserName, "passwd":PassWord},
+        uploadAsync:true,
+        browseClass: "btn btn-primary", //按钮样式
+        showCaption: false,//是否显示标题
+        maxFileCount: 1, //表示允许同时上传的最大文件个数
+        enctype: 'multipart/form-data',
+        validateInitialCount:true,
+    })
 }
+$("#image_upload_area").on("fileuploaded", function (event, data, previewId, index) {
 
+
+});
+
+function SetImage(){
+    $("#image_upload_area").removeClass("hidden");
+}
+function NotSetImage(){
+    $("#image_upload_area").addClass("hidden");
+}
 function checkAgree(){
     if( $(" p[ id='warning-text'] ").css("visibility") == "visible"){
         $(" p[ id='warning-text'] ").css("visibility", "hidden");
@@ -129,24 +128,44 @@ function checkAgree(){
 }
 
 function changeSearchKey(e) {
-
-    $('#SearchBar').find('li').each(function(){
+    $("[role='presentation']").removeClass('active');
+    /*$('#SearchBar').find('li').each(function(){
         console.log("1");
         $(this).removeClass("active");
-    })
+    })*/
     $(e).addClass("active");
-
 }
 
 function Search() {
     var i = 1;
     var target = 0 ;
-    $('#SearchBar').find('li').each(function () {
+    /*$('#SearchBar').find('li').each(function () {
         if ($(this).hasClass("active")) {
             target = i;
         }
         i++;
-    })
+    })*/
+
     //console.log(target);
+    var type = $("[role='presentation'].active").text();
+    console.log(type);
+    var typenum;
+    switch(type){
+        case '科技成果':
+            typenum = 1;
+            break;
+        case '专家':
+            typenum = 2;
+            break;
+        case '科研机构':
+            typenum = 3;
+            break;
+        case '标签':
+            typenum = 4;
+            break;
+    }
 
 }
+
+
+
