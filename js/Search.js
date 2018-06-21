6339c044b28d72668509305f8c5bb519c2c8120b
GreeeenPepper <wuyize1994@163.com>
2018-06-21 14:18:45 +0800

    Picture Upload FrontEnd

--- js/Search.js
+++ js/Search.js
@@ -1,28 +1,46 @@
+
+var myList = [
+    {
+        id:1,
+        name: 'test1',
+        author: 'test1a',
+        institution: 'test1t'
+    },
+    {
+        id:2,
+        name: 'test2',
+        author: 'test2a',
+        institution: 'test2t'
+    },
+    {
+        id:3,
+        name: 'test3',
+        author: 'test3a',
+        institution: 'test3t'
+    }
+];
+
 new Vue({
     el: '#SearchResultList',
     data: {
-        resultList: [
-            {
-                name: 'test1',
-                author: 'test1a',
-                institution: 'test1t'
-            },
-            {
-                name: 'test2',
-                author: 'test2a',
-                institution: 'test2t'
-            },
-            {
-                name: 'test3',
-                author: 'test3a',
-                institution: 'test3t'
-            }
-        ]
+        url :'test',
+        resultList: myList
     },
     created: function () {
         var url = "";
-        $.get(url, function (data) {
+        var a = {
+            id:4,
+            name:'test4',
+            author:'test4a',
+            institution:'test4t'
+        }
+        myList.push(a);
+        for(result in myList){
+            myList[result].id  = 'resource.html?resource_id='+ myList[result].id;
+            console.log( myList[result].id);
+        }
+       /* $.get(url, function (data) {
 
-        })
+        })*/
     }
 })
